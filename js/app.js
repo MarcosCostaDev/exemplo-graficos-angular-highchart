fd3c2b43c1269f3e7aa2e87df8aac21a36196b3a
Marcos Costa <marcos.lcosta@live.com>
2017-01-10 21:59:18 -0300

    atualizando botão de mostrar próximo grafico

--- js/app.js
+++ js/app.js
@@ -8,12 +8,13 @@ testechart.controller('testecontroller', ['$scope', '$http', function ($scope, $
     { "id": "spline", "title": "Smooth line" },
     { "id": "area", "title": "Area" },
     { "id": "areaspline", "title": "Smooth area" },
-    { "id": "column", "title": "Column" },
+    { "id": "column", "title": "Column" },  
     { "id": "bar", "title": "Bar" },
     { "id": "pie", "title": "Pie" },
     { "id": "scatter", "title": "Scatter" },
     { "id": "funnel", "title": "funnel" },
-
+    { "id": "waterfall", "title": "waterfall" },
+    { "id": "columnrange", "title": "columnrange" },
   ];
 
   //estilos suportados
@@ -54,13 +55,14 @@ testechart.controller('testecontroller', ['$scope', '$http', function ($scope, $
 
     $scope.AtualGrafico = $scope.chartTypes[$scope.contador].title;
 
-    if ($scope.chartConfig.chart.type == "pie" || $scope.chartConfig.chart.type == "funnel" ) {
+    if ($scope.chartConfig.chart.type == "pie" || $scope.chartConfig.chart.type == "funnel") {
       $scope.chartConfig = {
         chart: {
           plotBackgroundColor: null,
           plotBorderWidth: null,
           plotShadow: false,
-          type: $scope.chartConfig.chart.type
+          type: $scope.chartConfig.chart.type,
+          inverted: false
         },
         title: {
           text: 'Browser market shares January, 2015 to May, 2015'
@@ -108,11 +110,76 @@ testechart.controller('testecontroller', ['$scope', '$http', function ($scope, $
         }]
       }
     }
+    else if ($scope.chartConfig.chart.type == "columnrange") {
+      $scope.chartConfig = {
+        chart: {
+          type: 'columnrange',
+          inverted: true
+        },
+
+        title: {
+          text: 'Temperature variation by month'
+        },
+
+        subtitle: {
+          text: 'Observed in Vik i Sogn, Norway'
+        },
+
+        xAxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+
+        yAxis: {
+          title: {
+            text: 'Temperature ( °C )'
+          }
+        },
+
+        tooltip: {
+          valueSuffix: '°C'
+        },
+
+        plotOptions: {
+          columnrange: {
+            dataLabels: {
+              enabled: true,
+              formatter: function () {
+                return this.y + '°C';
+              }
+            }
+          }
+        },
+
+        legend: {
+          enabled: false
+        },
+
+        series: [{
+          name: 'Temperatures',
+          data: [
+            [-9.7, 9.4],
+            [-8.7, 6.5],
+            [-3.5, 9.4],
+            [-1.4, 19.9],
+            [0.0, 22.6],
+            [2.9, 29.5],
+            [9.2, 30.7],
+            [7.3, 26.5],
+            [4.4, 18.0],
+            [-3.1, 11.4],
+            [-5.2, 10.4],
+            [-13.5, 9.8]
+          ]
+        }]
+      }
+    }
     else {
+     
       $scope.chartConfig = {
 
         chart: {
-          type: $scope.chartTypes[$scope.contador].id
+          type: $scope.chartTypes[$scope.contador].id,
+          inverted: false
         },
         plotOptions: {
           series: {
